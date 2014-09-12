dd4dc6683905ad7b110dbef4560338f6bd9e9bcf
Dallin Scherbel <scherpa.bball@gmail.com>
2014-09-12 10:40:55 -0600

    Initial files commit

---
+++ src/GoReact/Test.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace GoReact;
+
+/**
+ * Title here...
+ *
+ * @link    goreact.com
+ * @author  Dallin Scherbel <dallin@speakworks.com>
+ */
+class Test
+{
+    public function test() {
+        return "test";
+    }
+}
